32d19e20a052de42b2c26b94fe2b6a1e13ea55c0
Fabian-MG <mirandafabian1510@gmail.com>
2021-03-15 19:30:31 -0600

    Añadiendo clase para manejar peticiones

---
+++ src/main/java/itq/dist/Request.java
@@ -0,0 +1,18 @@
+package com.itq.pd;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Request implements Serializable{
+
+    public UUID ID;
+    public int liters;
+    public int tank;
+    
+    public Request(int liters, int tank) {
+    	this.tank = tank;
+    	this.liters = liters;
+    	this.ID = UUID.randomUUID();
+    }
+    
+}
